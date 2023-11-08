2029f019af62d66cf0a4d71299242578447c396b
zaidaamosquera <zaidayin56@gmail.com>
2023-11-08 10:23:03 -0500

    Por fin descargo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,15 +5,16 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Trabajo con Solorzanis</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
 
       
     </head>
     <body>
-        Hola Solorzanis, vamos a hacer el trabajo
+         <div>
+            <div>SZ</div>
+            <div> Peliculas  Series Generos</div>
+            
+        </div>
         
         
     </body>
